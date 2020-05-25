b4efa9b36964fc175cd87d1b700204f9492294b2
lorenzo <lor90td@gmail.com>
2020-05-24 23:32:18 -0500

    Percent pipe demostration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,6 @@ export class AppComponent {
   public name: string = "Capitan América";
   public array = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
   public PI: number = Math.PI;
+  public percent: number = 0.525;
 
 }
