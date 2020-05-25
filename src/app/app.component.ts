a73208e0ec1f74d03a1b4d5696b0cd25484c2927
lorenzo <lor90td@gmail.com>
2020-05-24 23:41:38 -0500

    Currency demostration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,6 @@ export class AppComponent {
   public array = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
   public PI: number = Math.PI;
   public percent: number = 0.525;
+  public salary: number = 3343.35;
 
 }
